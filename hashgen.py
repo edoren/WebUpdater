2cda89bfb6beed9744735798f8e425be3a0af2b1
Manuel Fernando Sabogal Ocampo <mfer32@gmail.com>
2013-05-04 19:25:08 -0500

    hasgen.py now only responsible for opening and closing the hash files.

--- hashgen.py
+++ hashgen.py
@@ -14,30 +14,16 @@ def __sha1_hash(filepath):
         f.close()
     return sha1.hexdigest()
 
-def getHash(rootpath):
-    hashDict = {}
+def getFileHash(file_path):
+    return __sha1_hash(file_path)
 
-    for (path, dirs, files) in os.walk(rootpath):
-        for filename in files:
-            hashDict[os.path.join(path, filename).split(rootpath)[1]] = __sha1_hash(os.path.join(path, filename))
-            print(os.path.join(path, filename).split(rootpath)[1])
-    
-    hash_file = open('updater.dat', 'wb')
-    pickle.dump(hashDict, hash_file)
+def openHashDict(data_file):
+    pkl_file = open(data_file, 'rb')
+    hashdict = pickle.load(pkl_file)
+    pkl_file.close()
+    return hashdict
+
+def saveHashDict(hash_dict, save_directory=''):
+    hash_file = open(os.path.join(save_directory, 'updater.dat'), 'wb')
+    pickle.dump(hash_dict, hash_file)
     hash_file.close()
-
-    # pkl_file = open('updater.dat', 'rb')
-    # mydict2 = pickle.load(pkl_file)
-    # pkl_file.close()
-
-    print(hashDict)
-
-def main(rootpath):
-    getHash(rootpath)
-
-if __name__ == '__main__':
-    try:
-        rootpath = sys.argv[1]
-        main(rootpath)
-    except:
-        print("Falta un argumento")
